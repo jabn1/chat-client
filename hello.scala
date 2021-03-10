6b3b94a3cfec991e50ffc2def50fb050c7a1e05d
Jose <jose.berges23@gmail.com>
2021-03-09 22:07:44 -0400

    Agregar cambios

--- hello.scala
+++ hello.scala
@@ -2,11 +2,16 @@ import java.net._
 import java.io._
 import scala.io._
 import scala.io.StdIn.readLine
+import scala.concurrent.Future
+
+
 
 val s = new Socket(InetAddress.getByName("localhost"), 5000)
 lazy val in = new BufferedSource(s.getInputStream()).getLines()
 val out = new PrintStream(s.getOutputStream())
 
+var last = ""
+last= null
 var connected = false
 var user = ""
 val commands = Map(
@@ -16,55 +21,79 @@ val commands = Map(
     "logout" -> "/CLOSE"
 )
 
-while(true){
-    while(!connected){
-        val input = readLine("Introduzca su usuario: ")
-        connected = connect(input) == "Ok"
-        if(connected){
-            user = input
-            println("Connectado exitosamente")
-            getUsers()
-        }else{
-            println("Intentelo nuevamente")
-        }
-    }
-
+while(!connected){
+    val input = readLine("Introduzca su usuario: ")
+    connected = connect(input) == "Ok"
     if(connected){
-        val input = readLine("Mensaje a Enviar (username:message): ") // username:asdlfjjsssf
-        sendMessage(input)
-
+        user = input
+        println("Connectado exitosamente")
+        getUsers()
+        println("Connected users: " + in.next())
+    }else{
+        println("Intentelo nuevamente")
     }
 }
-// while(true){
 
-// }
+implicit val ec: scala.concurrent.ExecutionContext = scala.concurrent.ExecutionContext.global
+val future = Future {
+    val result = getInput()
+    result
+}
+
+while(true){
+    if(connected){
+        val input = readLine("Mensaje a Enviar (username:message) or (command): ") // username:asdlfjjsssf
+        sendMessage(input)
+    }
+}
+
 def getValue(x: Option[String]) = x match { case Some(s) => s case None => "" }
+
+def getInput() : Boolean = {
+    while(true){
+        val next = in.next()
+        if(next.slice(0,5) == "/CHAT"){
+            println("\nUser message:" + next)
+        }else if(last == "getUsers") {
+            if (next != "Empty"){
+                println("\nConnected users: " + next)
+            }
+        }else if(last == "logout"){
+            if(next == "Ok"){
+                System.exit(0)
+            }
+        }
+    }
+    return true
+}
 
 def connect(user: String) : String = {
     val connectCommand = getValue(commands.get("login")).replace("$username", user)
     out.println(connectCommand)
     out.flush()
 
-    val response = in.next()
+    val response = in.next
 
     return response
 }
 
-def getUsers() : Boolean = {
+def getUsers() = {
     val getUsersCommand = getValue(commands.get("getUsers"))
     out.println(getUsersCommand)
     out.flush()
+    last = "getUsers"
 
-    val response = in.next()
-
-    if (response != "Empty"){
-        println("Connected users: " + response)
-    }
-
-    return (response != "Empty")
 }
 
-def sendMessage(inputData: String) : String = {
+def logout() = {
+    val getUsersCommand = getValue(commands.get("logout"))
+    out.println(getUsersCommand)
+    out.flush()
+    last = "logout"
+
+}
+
+def sendMessage(inputData: String) = {
     val data = inputData.split(":")
     if(data.size > 1 ){
 
@@ -74,11 +103,21 @@ def sendMessage(inputData: String) : String = {
         val chatCommand = getValue(commands.get("chat")).replace("$username", user).replace("$message", message)
         out.println(chatCommand)
         out.flush()
-
-
+        last = "chat"
     }
-
-    val response = in.next()
-    return response
+    else{ //comandos
+        val command = data(0)
+        if(command == "userlist"){
+            getUsers()
+            last = "getUsers"
+        }
+        else if(command == "quit"){
+            logout()
+            last = "logout"
+        }else{
+            last = ""
+            println("Invalid command")
+        }
+    }
 }
 s.close()
